09ffcb14bd6ac9da291f584e14ba95bb275d11a2
Dhruv Dattani <dhruvd22@gmail.com>
2023-01-23 15:37:41 -0500

    Update Python Quiz.py

--- Python Quiz.py
+++ Python Quiz.py
@@ -3,7 +3,7 @@ import random
 
 print ("Welcome to my first python quiz")
 play = input('Are you ready to play? ')
-
+rand = random.randint(1,300)
 if play != "yes":
     quit()
 print("Alright lets play")
@@ -15,7 +15,6 @@ if A1 == "Washington, DC":
 else:
     print("You're a dumb son of a bitch")
     print("Sorry for being harsh here is a present!")
-    rand = random.randint(1,300)
     print("$", rand)
 A2 = input('Whats 25 times 25? ')
 
@@ -25,6 +24,12 @@ if A2 == "625":
     exit()
 else:
     print("Damn you get one more chance with a final questions")
+    A3 = input('What is your name? ' )
+    if (A3!=" "):
+        print('You automatically win take your', rand, 'and get outta here!!')
+    else:
+        print('Damn how you mess it up like that???')
+        exit()
 
 
 
